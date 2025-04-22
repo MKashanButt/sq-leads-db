93cf2a2bc7ab365fccb6e4deac1d5698393eaa2c
MKashanButt <kashanbutt566@gmail.com>
2025-04-22 22:38:43 +0500

    removed nullable and added default

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->enum('role', ['admin', 'manager', 'agent', 'center'])->nullable();
+            $table->enum('role', ['admin', 'manager', 'agent', 'center'])->default('agent');
             $table->rememberToken();
             $table->timestamps();
         });
